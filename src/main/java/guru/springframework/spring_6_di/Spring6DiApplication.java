b3e852867e566740b2ad7dde0ed4ff315072c7b0
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-03-24 19:24:43 +0000

    The Spring Context

--- src/main/java/guru/springframework/spring_6_di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring_6_di/Spring6DiApplication.java
@@ -1,13 +1,22 @@
 package guru.springframework.spring_6_di;
 
+import guru.springframework.spring_6_di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("In Main Method");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
