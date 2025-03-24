3e09ff2b1fe7134dc4e282a3fd5d33ac8c9408f5
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-03-24 19:35:58 +0000

    Basics of Dependency Injection

--- src/test/java/guru/springframework/spring_6_di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring_6_di/Spring6DiApplicationTests.java
@@ -1,13 +1,33 @@
 package guru.springframework.spring_6_di;
 
+import guru.springframework.spring_6_di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
 	@Test
-	void contextLoads() {
+	void testAutowireOfController() {
+		System.out.println(myController.sayHello());
 	}
+
+	@Test
+	void testGetControllerFromCtx(){
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void contextLoads() {}
 
 }
