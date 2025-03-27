0fe33c1c0bc3f5bdd8f2069e6a767e2a5c8e2ebd
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-03-27 11:45:57 +0000

    Default Profile

--- src/main/java/guru/springframework/spring_6_di/services/i18n/SpanishGreetingService.java
+++ src/main/java/guru/springframework/spring_6_di/services/i18n/SpanishGreetingService.java
@@ -4,7 +4,7 @@ import guru.springframework.spring_6_di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile(("ES"))
+@Profile("ES")
 @Service("i18NService")
 public class SpanishGreetingService implements GreetingService {
     @Override
