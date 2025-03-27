ae46a940ac8aad522996355b70664f2dddab88df
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-03-27 11:35:27 +0000

    Spring Profiles

---
+++ src/main/java/guru/springframework/spring_6_di/services/i18n/SpanishGreetingService.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring_6_di.services.i18n;
+
+import guru.springframework.spring_6_di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile(("ES"))
+@Service("i18NService")
+public class SpanishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
--- src/test/java/guru/springframework/spring_6_di/controllers/MyControllerTest.java
+++ src/test/java/guru/springframework/spring_6_di/controllers/MyControllerTest.java
@@ -2,8 +2,6 @@ package guru.springframework.spring_6_di.controllers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MyControllerTest {
 
     @Test
